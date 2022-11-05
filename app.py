e39a0d5a7639a5fd084191635e14ccf8294b3274
csiztom <tom.csiz@gmail.com>
2022-11-05 13:24:33 +0100

    revised load-in

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask
-from tools.load_pandas import load_dataset
+from tools.load_pandas import load_days, load_minutes
 from dotenv import load_dotenv
 import datetime
 
 load_dotenv()
 app = Flask(__name__)
-df = load_dataset()
+df_minutes = load_minutes()
+df_days = load_days()
 
 @app.route('/api/')
 def hello_world():
-    oblasts = df.oblast.unique()
-    return 'Hello World!<br>oblasts: ' + str(oblasts)
+    origin_countries = df_days['origin country'].unique()
+    oblasts = df_minutes.oblast.unique()
+    return 'Hello World!<br>oblasts: ' + str(oblasts) + ' ' + str(origin_countries)
 
 
 if __name__ == '__main__':
